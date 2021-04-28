ed69c9196c54750511f8f83b3465793db892b893
Anant Kanchan <anant4265@gmail.com>
2021-04-28 19:07:12 +0530

    initial commit

---
+++ Heineken/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Heineken
+//
+//  Created by Anant Kanchan on 28/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Heineken/HeinekenApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeinekenApp.swift
+//  Heineken
+//
+//  Created by Anant Kanchan on 28/04/21.
+//
+
+import SwiftUI
+
+@main
+struct HeinekenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
