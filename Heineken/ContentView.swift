d60c33661dcd3d330757f62b8e524c9832d2187a
Anant Kanchan <anant4265@gmail.com>
2021-05-04 17:04:24 +0530

    sf syadded

--- Heineken/ContentView.swift
+++ Heineken/ContentView.swift
@@ -17,17 +17,24 @@ struct ContentView: View {
         GeometryReader{ geometry in
             VStack{
                 Spacer()
-                LazyVGrid(columns: columns) {
+                LazyVGrid(columns: columns,spacing:5) {
                     ForEach(0..<9) { i in
                         ZStack{
                             Circle()
                                 .foregroundColor(.red).opacity(0.5)
+                                .frame(width: geometry.size.width/3 - 15, height: geometry.size.width/3 - 15)
                             
+                            
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundColor(.white)
                         }
                     }
                 }
                 Spacer()
             }
+            .padding()
         }
     }
 }
