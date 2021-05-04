feaa6c4fad6b68d4526a8c745ae1f28f2bc670fa
Anant Kanchan <anant4265@gmail.com>
2021-05-04 17:17:46 +0530

    raw logic added

--- Heineken/ContentView.swift
+++ Heineken/ContentView.swift
@@ -7,11 +7,16 @@
 
 import SwiftUI
 
-let columns: [GridItem] = [GridItem(.flexible()),
-                           GridItem(.flexible()),
-                           GridItem(.flexible()),]
 
 struct ContentView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible()),]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumansTurn = true
+    
     var body: some View {
         
         GeometryReader{ geometry in
@@ -25,10 +30,14 @@ struct ContentView: View {
                                 .frame(width: geometry.size.width/3 - 15, height: geometry.size.width/3 - 15)
                             
                             
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[i]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 40, height: 40)
                                 .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            moves[i] = Move(player: isHumansTurn ? .human : .computer, boardIndex: i)
+                            isHumansTurn.toggle()
                         }
                     }
                 }
@@ -38,6 +47,24 @@ struct ContentView: View {
         }
     }
 }
+
+
+
+enum Player{
+    case human,computer
+}
+
+struct Move{
+    let player:Player
+    let boardIndex:Int
+    
+    var indicator:String{
+        return player == .human ? "xmark" : "circle"
+    }
+}
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
