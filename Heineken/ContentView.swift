3a3a05676edd58e538243e41416669c5684d084c
Anant Kanchan <anant4265@gmail.com>
2021-05-07 19:48:01 +0530

    condi widone

--- Heineken/ContentView.swift
+++ Heineken/ContentView.swift
@@ -41,11 +41,18 @@ struct ContentView: View {
                             isGameboardDisabled = true
                            
                             //check for win or draw
+                            if checkWinCondition(for: .human, in: moves){
+                                print("Human wins")
+                            }
                             
                             DispatchQueue.main.asyncAfter(deadline: .now() + 0.5){
                                 let computerPosition = determineComputerMovePosition(in: moves)
                                 moves[computerPosition] = Move(player: .computer, boardIndex: computerPosition)
                                 isGameboardDisabled = false
+                                
+                                if checkWinCondition(for: .computer, in: moves){
+                                    print("Computer wins")
+                                }
                             }
                         }
                     }
@@ -68,6 +75,18 @@ struct ContentView: View {
             movePosition = Int.random(in: 0..<9)
         }
         return movePosition
+    }
+    
+    func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
+        let winPatterns: Set<Set<Int>> = [[0, 1, 2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        
+        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
+        let playerPositions = Set(playerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions){
+            return true
+        }
+        return false
     }
 }
 
